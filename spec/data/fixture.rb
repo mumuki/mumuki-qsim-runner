fa587ae6c17f054c4a292f5704127adb864ff16f
Charlyzzz <erwincdl@gmail.com>
2017-02-17 00:05:59 -0300

    Specs and fixtures refactor

--- spec/data/fixture.rb
+++ spec/data/fixture.rb
@@ -1,32 +1,10 @@
+require_relative 'q1_ok_program'
+require_relative 'r1_plus_r2_program'
+require_relative 'times_two_program'
+require_relative 'times_three_program'
+require_relative 'r1_times_r2'
+
 module Fixture
-  def q1_ok_program
-    <<~QSIM
-      MOV R3, 0x0003
-      MOV R5, 0x0004
-      ADD R3, R5
-    QSIM
-  end
-
-  def sum_r1_r2_program
-    'ADD R1, R2'
-  end
-
-  def times_two_usage_program
-    <<~QSIM
-      MOV R1, 0x0002
-      CALL timesTwo
-      CALL timesTwo
-    QSIM
-  end
-
-  def times_two_definition_program
-    <<~QSIM
-      timesTwo:
-      MUL R1, 0x0002
-      RET
-    QSIM
-  end
-
   def syntax_error_program
     'MOB R3, 0x0003'
   end
