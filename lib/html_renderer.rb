5b283865b177ad3917f1cfb7f85cba5f539f9b35
Charlyzzz <erwincdl@gmail.com>
2017-02-18 02:13:41 -0300

    Tweaked output layout and order

--- lib/html_renderer.rb
+++ lib/html_renderer.rb
@@ -1,13 +1,18 @@
+require 'active_support/inflector'
+
 module Qsim
   class HtmlRenderer
     def render(result, output)
       @output = output
-      @result = result
-      if output[:memory]
-        memory_range(output).each do |record|
-          field = to_4_digits_hex(record)
-          @result[:memory].merge!(field => '0000') { |_, old_value, _| old_value }
-        end
+      @result = {}
+      @layout = {}
+      output.keys.each do |output_key|
+        @result[output_key] = range(output_key, output)
+                                .map { |key| [key_for(output_key, key), '0000'] }
+                                .to_h
+                                .merge(result[output_key])
+                                .sort
+        @layout[output_key] = columns_number(output_key.length)
       end
       template_file.result binding
     end
@@ -18,14 +23,50 @@ module Qsim
       ERB.new File.read("#{__dir__}/view/records.html.erb")
     end
 
-    def to_4_digits_hex(number)
-      number.to_s(16).rjust(4, '0').upcase
+    def columns_number(size)
+      size < 7 ? size : 4
+    end
+
+    def to_memory(number)
+      number.to_s(16).rjust(4, '0').upcase.to_sym
+    end
+
+    def to_record(number)
+      "R#{number}".to_sym
+    end
+
+    def to_flag(number)
+      number.to_sym
+    end
+
+    def to_special_record(record)
+      record.to_sym
     end
 
     def memory_range(options)
       from = options[:memory][:from].to_hex
       to = options[:memory][:to].to_hex
       (from..to)
+    end
+
+    def range(output_key, output)
+      send("#{output_key}_range", output)
+    end
+
+    def key_for(output_key, key)
+      send("to_#{output_key}".singularize, key)
+    end
+
+    def records_range(_)
+      (0..7)
+    end
+
+    def flags_range(_)
+      %w(N Z V C).map(&:to_sym)
+    end
+
+    def special_records_range(_)
+      %w(SP PC IR).map(&:to_sym)
     end
   end
 end
--- spec/html_renderer_spec.rb
+++ spec/html_renderer_spec.rb
@@ -1,8 +1,16 @@
 describe Qsim::HtmlRenderer do
   describe '#render' do
-    it 'only renders desired categories' do
+    it 'renders memory by default' do
       expect(render).to include 'Records', 'R0', 'CAFE', 'R7', 'BABE'
       expect(render).not_to include 'Memory', 'Flags', 'Special', 'Symbols', 'SP', '0007'
+    end
+
+    it 'renders all categories' do
+      rendering = render(memory: { from: '0007', to: '000B' },
+                         flags: true,
+                         records: true,
+                         special_records: true)
+      expect(rendering).to include 'Memory', 'Flags', 'Special records', 'Records'
     end
 
     context 'when memory is specified' do
@@ -29,7 +37,7 @@ describe Qsim::HtmlRenderer do
       result = { records: { R0: 'CAFE', R7: 'BABE' },
                  memory: { '0007': '0001', '000A' => '00AA' },
                  flags: { N: 0, Z: 0, V: 0, C: 1 },
-                 special_recods: { SP: 'FFEF' } }
+                 special_records: { SP: 'FFEF' } }
       result.deep_symbolize_keys!
       Qsim::HtmlRenderer.new.render(result, output)
     end
